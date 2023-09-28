41dbbed28a6a5e97ff167c4ab3bfe71f05aedb47
Jackson Neuman <jacksonneuman@Jacksons-Air-4.attlocal.net>
2023-09-27 23:45:45 -0500

    added index.html script.js and style.css files

---
+++ script.js
@@ -0,0 +1,37 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+$(function () {
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+  //
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
+  // TODO: Add code to display the current date in the header of the page.
+});
+
+//Psuedo Code
+
+//Display today's date via DayJS
+
+//Need to add more hours/timeblocks for input in your HTML
+
+//Functionality for the SaveButton
+//Add event listener, need to be able to store and remove user input from local storage.
+
+//Create some sort of Timer Function
+  //loop through each time block...based upon current time it will know if it is past, present, or future
+
+//On page reload or refresh...any input in local storage needs to be rendered to the correct hour.
